12c4f6670461ae35663c20fab2b2f008efcaa542
str <timkakadirov96@gmail.com>
2024-08-09 13:34:08 +0500

    admin fix

--- core/custum_permission.py
+++ core/custum_permission.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-class LoggedSuperUser(LoginRequiredMixin,UserPassesTestMixin):
+class LoggedSuperUser(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.is_superuser
+        return self.request.user.is_staff
